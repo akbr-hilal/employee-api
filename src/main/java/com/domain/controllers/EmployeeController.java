2b287cd7df89742f9e6cfca9174455f7195e781e
akbr-hilal <dev.akbrhilal@gmail.com>
2022-09-07 07:18:15 +0700

    update 7

--- src/main/java/com/domain/controllers/EmployeeController.java
+++ src/main/java/com/domain/controllers/EmployeeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.domain.models.entities.Employee;
 import com.domain.services.EmployeeService;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
